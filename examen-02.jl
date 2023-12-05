af92aa8322b5d6dd0ca4d8316a42c5e515376551
Isaac <isaacmues@gmail.com>
2023-12-05 03:28:58 -0600

    Añade el código en Julia

---
+++ examen-02.jl
@@ -0,0 +1,23 @@
+using LinearAlgebra
+
+h = pi / 20
+
+# Construcción del esquema de diferencias finitas
+dv = (h^2 - 2) * ones(1,19) |> vec
+ev = ones(1,18) |> vec
+A  = SymTridiagonal(dv, ev)
+B = zeros(19)
+B[1] = -1.0
+B[19] = 1.0
+
+# Solución Numérica
+Y = A \ B
+
+# Solución Analítica
+X = h:h:pi-h
+U = cos.(X)
+
+println("Numérica","\t  ", "Analítica")
+for i in 1:1:19
+    println(Y[i],"\t", U[i])
+end
