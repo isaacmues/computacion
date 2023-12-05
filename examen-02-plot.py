c6bcfeb110a6d4e8830e0e6200a8bacd9fbf5e6d
Isaac <isaacmues@gmail.com>
2023-12-05 10:34:12 -0600

    Actualiza la gráfica con los resultados de Julia

--- examen-02-plot.py
+++ examen-02-plot.py
@@ -2,15 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Carga los datos
-input_data = np.loadtxt('resultados.dat', usecols=(0,1,2))
-x = input_data[:,0]
-y = input_data[:,1] # Solución numérica 
-u = input_data[:,2] # Solución analítica
+input_data_py = np.loadtxt('resultados.dat', usecols=(0,1,2))
+x = input_data_py[:,0]
+y = input_data_py[:,1] # Solución numérica 
+u = input_data_py[:,2] # Solución analítica
+y2 = np.loadtxt('resultados-julia.csv')
 
 # Hace la gráfica
 fig, ax = plt.subplots()
 ax.plot(x, u, label='Solución Analítica')
-ax.plot(x, y, label='Solución Numérica')
+ax.plot(x, y, label='Solución Numérica (Fortran)')
+ax.plot(x, y2, label='Solución Numérica (Julia)')
 ax.legend()
 ax.set_title('Diferencias Finitas')
 ax.set_xlabel=('x')
